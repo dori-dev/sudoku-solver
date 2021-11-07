d119147a231ebecbeef085934c73504702423763
mohammad <mr.dori.dev@gmail.com>
2021-11-07 20:03:21 +0330

    update draw function

--- functions.py
+++ functions.py
@@ -1,13 +1,25 @@
 """Project functions
 """
 from assets import puzzle, guess
+chars = ("|", "--")
 
 
 def draw():
-    for j in puzzle:
-        for i in j:
-            print(i, end=" ")
-        print("\n")
+    print("┌───────┬───────┬───────┐")
+    for i in range(9):
+        for j in range(9):
+            if j % 3 == 0:
+                print("│ ", end="")
+            print(f"{puzzle[i][j]} ", end="")
+        
+        print("│")
+        if i == 8:
+            print("└───────┴───────┴───────┘")
+        elif (i + 1) % 3 == 0:
+            print("├───────┼───────┼───────┤")
+
+
+
 
 
 def find_free():
