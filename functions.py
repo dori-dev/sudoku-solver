395033e72d6c28ec4440ac8dc472316ab78cf5bf
mohammad <mr.dori.dev@gmail.com>
2021-11-07 20:36:43 +0330

    created is_valid

--- functions.py
+++ functions.py
@@ -28,9 +28,20 @@ def find_free():
     return False
 
 
-def is_valid():
-    pass
+def is_valid(n, x, y):
+    for i in range(9):
+        if n in (puzzle[x][i], puzzle[i][y]):
+            return False
+
+    x_square = (x // 3) * 3
+    y_square = (y // 3) * 3
+
+    for i in range(x_square, x_square+3):
+        for j in range(y_square, y_square+3):
+            if puzzle[i][j] == n:
+                return False
+    return True
 
 
 def solve():
-    return True
+    pass
