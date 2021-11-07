41d1f486f47c60fdcb5b9b49c3725dcf5a0dccc6
mohammad <mr.dori.dev@gmail.com>
2021-11-07 19:33:39 +0330

    add module docstring

--- functions.py
+++ functions.py
@@ -1,11 +1,22 @@
+"""Project functions
+"""
+from assets import puzzle, guess
+
+
 def draw():
+    for j in puzzle:
+        for i in j:
+            print(i, end=" ")
+        print("\n")
+
+
+def find_free():
     pass
+
 
 def is_valid():
     pass
 
-def solve():
-    pass
 
-def find_free():
-    pass
+def solve():
+    return True
